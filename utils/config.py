047ede7760f33ee618c6d6bef95154cdfa1d66d5
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-05 21:44:16 -0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,7 @@
+class Config:
+    def __init__(self):
+        self.model_name = 'lvwerra/gpt2-imdb'
+        self.learning_rate = 1.41e-5
+        self.log_with = 'wandb'
+        # Add other configurations as necessary
 
